f4621e9da7183c17cb367e2be629c75e95f59057
Serdar D7K <serdarozkan41@gmail.com>
2023-01-10 09:27:07 +0300

    goto transfer timer

--- Program.cs
+++ Program.cs
@@ -5,76 +5,87 @@ using System.Data.SqlClient;
 
 try
 {
-Start:
-    Console.Clear();
-    Console.WriteLine($"----------------------------------START----------------------------------");
 
     AppSettings appSettings = JsonConvert.DeserializeObject<AppSettings>(File.ReadAllText("appsettings.json"));
 
-    Console.WriteLine($"Backup Folder: {appSettings.BackupFolder}");
-    List<string> zipFiles = new List<string>();
-    zipFiles = new List<string>();
+    var timer = new PeriodicTimer(TimeSpan.FromSeconds(15));
 
-    foreach (var company in appSettings.Companies)
+    while (await timer.WaitForNextTickAsync())
     {
+        timer.Dispose();
 
-        Console.WriteLine("Started...");
-        try
+        Console.Clear();
+        Console.WriteLine($"----------------------------------START----------------------------------");
+
+
+        Console.WriteLine($"Backup Folder: {appSettings.BackupFolder}");
+        List<string> zipFiles = new List<string>();
+
+        foreach (var company in appSettings.Companies)
         {
-            string backupFileName = $"{appSettings.FilePrefix}_{company.DatabaseName}_{DateTime.Now.ToString().Replace(".", "_").Replace(":", "_").Replace(" ", "_")}.bak";
-            string backupFile = $"{appSettings.BackupFolder}\\{backupFileName}";
-            string backupQuery = appSettings.BackupQuery.Replace("@DBNAME", company.DatabaseName).Replace("@BFOLDER", $"{backupFile}");
-            string zipFile = $"{appSettings.TempFolder}\\{backupFileName.Replace(".bak", ".zip")}";
 
-            using (SqlConnection sqlConnection = new SqlConnection(company.ConnectionString))
+            Console.WriteLine("Started...");
+            try
             {
-                sqlConnection.Open();
-                using (SqlCommand sqlCommand = new SqlCommand(backupQuery, sqlConnection))
+                string backupFileName = $"{appSettings.FilePrefix}_{company.DatabaseName}_{DateTime.Now.ToString().Replace(".", "_").Replace(":", "_").Replace(" ", "_")}.bak";
+                string backupFile = $"{appSettings.BackupFolder}\\{backupFileName}";
+                string backupQuery = appSettings.BackupQuery.Replace("@DBNAME", company.DatabaseName).Replace("@BFOLDER", $"{backupFile}");
+                string zipFile = $"{appSettings.TempFolder}\\{backupFileName.Replace(".bak", ".zip")}";
+
+                using (SqlConnection sqlConnection = new SqlConnection(company.ConnectionString))
                 {
-                    sqlCommand.CommandTimeout = 5000;
-
-                    var result = sqlCommand.ExecuteNonQuery();
-
-                    if (result == 0)
-                        Console.WriteLine($"{company.DatabaseName} backup file don't created.");
-                    else
-                        Console.WriteLine($"{company.DatabaseName} backup file created.");
-
-                    using (ZipFile zip = new ZipFile())
+                    sqlConnection.Open();
+                    using (SqlCommand sqlCommand = new SqlCommand(backupQuery, sqlConnection))
                     {
-                        zip.Password = appSettings.ZipPassword;
-                        zip.Comment = "This zip was created at " + DateTime.Now.ToString("G");
-                        zip.UseZip64WhenSaving = Zip64Option.AsNecessary;
-                        zip.AddFile(backupFile, @"\");
-                        zip.Save(zipFile);
+                        sqlCommand.CommandTimeout = 5000;
+
+                        var result = sqlCommand.ExecuteNonQuery();
+
+                        if (result == 0)
+                            Console.WriteLine($"{company.DatabaseName} backup file don't created.");
+                        else
+                            Console.WriteLine($"{company.DatabaseName} backup file created.");
+
+                        using (ZipFile zip = new ZipFile())
+                        {
+                            zip.Password = appSettings.ZipPassword;
+                            zip.Comment = "This zip was created at " + DateTime.Now.ToString("G");
+                            zip.UseZip64WhenSaving = Zip64Option.AsNecessary;
+                            zip.AddFile(backupFile, @"\");
+                            zip.Save(zipFile);
+                        }
+
+                        Console.WriteLine($"Compression ready.");
+                        File.Delete(backupFile);
+                        Console.WriteLine($"Backup File Deleted.");
+
+                        zipFiles.Add(zipFile);
                     }
-
-                    Console.WriteLine($"Compression ready.");
-                    File.Delete(backupFile);
-                    Console.WriteLine($"Backup File Deleted.");
-
-                    zipFiles.Add(zipFile);
+                    sqlConnection.Close();
                 }
             }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
-        catch (SqlException e)
+
+        Console.WriteLine($"Starting backup and compression ready installation for all companies.");
+
+        foreach (var zipFile in zipFiles)
         {
-            Console.WriteLine(e.ToString());
+            //UPLOAD
+            Console.WriteLine(zipFile);
         }
+
+        Console.WriteLine($"Waiting.... {appSettings.WaitMinute} Minute.");
+        Console.WriteLine($"Last Run Time: {DateTime.Now.ToString("dd.MM.yyyy HH:mm")}");
+
+        Console.WriteLine($"----------------------------------END----------------------------------");
+
+        timer = new PeriodicTimer(TimeSpan.FromMinutes(appSettings.WaitMinute));
     }
 
-    Console.WriteLine($"Starting backup and compression ready installation for all companies.");
-
-    foreach (var zipFile in zipFiles)
-    {
-        Console.WriteLine(zipFile);
-    }
-
-    Console.WriteLine($"Waiting.... {appSettings.WaitMinute} Minute.");
-
-    Console.WriteLine($"----------------------------------END----------------------------------");
-    await Task.Delay(TimeSpan.FromMinutes(appSettings.WaitMinute));
-    goto Start;
 }
 catch (Exception ex)
 {
